32d8b8efb4f49fda573e9a44a485967e3b76a68d
Timothy Sayre <buckyinsfo@gmail.com>
2016-10-23 17:22:37 -0700

    Copy text change.

--- javascript/bundle.js
+++ javascript/bundle.js
@@ -23507,7 +23507,7 @@
 	'use strict';
 	
 	module.exports = {
-	  summary: 'Hi I’m Lindsay, an Ohio State University student with a passion for marketing and entrepreneurship. I have worked at a startup, a multi-billion dollar corporation, and studied everything in between. My experience spans across a wide range of products and services, so I specialize in jumping in with both feet and getting up to speed quickly. I love a challenge and thrive in a dynamic work environment where I get to wear many hats.',
+	  summary: 'I’m Lindsay, an Ohio State University student with a passion for marketing and entrepreneurship. I have worked at a startup, a multi-billion dollar corporation, and studied everything in between. My experience spans across a wide range of products and services, so I specialize in jumping in with both feet and getting up to speed quickly. I love a challenge and thrive in a dynamic work environment where I get to wear many hats.',
 	  project1: 'A handspun framework in the style of Rails.',
 	  project2: 'A fruit foraging app built on Rails with a React/Redux frontend.',
 	  project3: 'A simple live chat app written in Node using socket.io.',
